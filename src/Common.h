9ed7dc63ad87db46daa258fc23eaf6bbf66701af
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-06-27 19:58:34 -0400

    Add Common.h header file with constants and includes

---
+++ src/Common.h
@@ -0,0 +1,18 @@
+#ifndef MOD_GUILDHOUSE_COMMON_H
+#define MOD_GUILDHOUSE_COMMON_H
+
+#include "ScriptMgr.h"
+#include "Creature.h"
+#include "Player.h"
+#include "Chat.h"
+#include "Config.h"
+#include "Guild.h"
+#include "GuildMgr.h"
+#include "World.h"
+#include "ObjectMgr.h"
+#include "DatabaseEnv.h"
+
+constexpr uint32 GH_GOSSIP_BUY = 1;
+constexpr uint32 GH_GOSSIP_TELEPORT = 2;
+
+#endif
