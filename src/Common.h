59f85f1ff5c94a4e6f02965cb4a22ba6bae92cd0
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-06-27 22:16:30 -0400

    Add NPC entry and position constants to Common.h
    Added new constants for NPC entry and position coordinates.

--- src/Common.h
+++ src/Common.h
@@ -8,11 +8,19 @@
 #include "Config.h"
 #include "Guild.h"
 #include "GuildMgr.h"
-#include "World.h"
-#include "ObjectMgr.h"
 #include "DatabaseEnv.h"
+#include "Position.h"
 
 constexpr uint32 GH_GOSSIP_BUY = 1;
 constexpr uint32 GH_GOSSIP_TELEPORT = 2;
+
+constexpr uint32 GH_NPC_ENTRY = 900000;
+
+constexpr uint32 GH_MAP = 1;
+
+constexpr float GH_X = 16222.57f;
+constexpr float GH_Y = 16265.91f;
+constexpr float GH_Z = 13.21f;
+constexpr float GH_O = 0.0f;
 
 #endif
