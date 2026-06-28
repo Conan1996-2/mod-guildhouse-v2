02d412c33d7671725af5d15e1f6aff48eb1336af
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-06-27 20:03:09 -0400

    Add Guild House Broker NPC to creature_template

---
+++ data/sql/db-world/0002_guildhouse_npc.sql
@@ -0,0 +1,4 @@
+DELETE FROM creature_template WHERE entry=900000;
+
+INSERT INTO creature_template (entry, name, subname, minlevel, maxlevel, faction, npcflag, unit_class, type, scale, speed_walk, speed_run, AIName, ScriptName)
+VALUES (900000, 'Guild House Broker', 'Guild Services', 80, 80, 35, 1, 1, 7, 1, 1, 1.14286, '', 'npc_guildhouse');
