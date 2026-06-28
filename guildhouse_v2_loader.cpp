1b8e876b23bcda8eba8526e28c9832bd9b64de63
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-06-27 22:28:07 -0400

    Delete guildhouse_v2_loader.cpp

--- guildhouse_v2_loader.cpp
+++
@@ -1,7 +0,0 @@
-// Forward declaration of your main script function
-void Addmod_guildhouse_v2_Scripts();
-
-// The core calls this automatically based on your folder name: mod-guildhouse-v2
-void Addmod_guildhouse_v2Scripts() {
-    Addmod_guildhouse_v2_Scripts();
-}
