e7e7f628deb6bf137093eadcb12e4428cc95aa42
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-06-27 22:18:24 -0400

    Refactor GuildHouseMgr by removing GuildHouseData
    Removed GuildHouseData struct and updated method signatures.

--- src/GuildHouseMgr.h
+++ src/GuildHouseMgr.h
@@ -3,30 +3,25 @@
 
 #include "Common.h"
 
-struct GuildHouseData
-{
-    uint32 GuildId;
-    uint32 OwnerGuid;
-    uint32 Map;
-    float X;
-    float Y;
-    float Z;
-    float O;
-};
-
 class GuildHouseMgr
 {
-  public:
-      static GuildHouseMgr* instance();
-      void Load();
-      bool HasGuildHouse(uint32 guildId) const;
-      const GuildHouseData* GetGuildHouse(uint32 guildId) const;
-      bool Purchase(Player* player);
-      bool Teleport(Player* player);
-  
-  private:
-      GuildHouseMgr() = default;
-      std::unordered_map<uint32, GuildHouseData> _houses;
+public:
+    static GuildHouseMgr* instance();
+
+    void Load();
+
+    bool HasHouse(uint32 guildId) const;
+
+    bool Purchase(Player* player);
+
+    bool Teleport(Player* player);
+
+    uint32 GetGuildPhase(uint32 guildId) const;
+
+private:
+    GuildHouseMgr() = default;
+
+    std::unordered_set<uint32> _guilds;
 };
 
 #define sGuildHouseMgr GuildHouseMgr::instance()
