e7c17bf34c322d8a97d3f6fb24bad257e7e184b3
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-06-29 16:49:37 -0400

    Refactor Load method for GuildHouseConfig

--- src/GuildHouseConfig.cpp
+++ src/GuildHouseConfig.cpp
@@ -10,33 +10,13 @@ GuildHouseConfig& GuildHouseConfig::Instance()
 
 void GuildHouseConfig::Load()
 {
-    _enabled = sConfigMgr->GetOption<bool>(
-        "GuildHouse.Enable",
-        false);
-
-    _free = sConfigMgr->GetOption<bool>(
-        "GuildHouse.Free",
-        false);
-
-    _loadAll = sConfigMgr->GetOption<bool>(
-        "GuildHouse.LoadAll",
-        false);
-
-    _houseCost = sConfigMgr->GetOption<uint64_t>(
-        "GuildHouse.Cost.House",
-        1000000ULL);
-
-    _npcCost = sConfigMgr->GetOption<uint64_t>(
-        "GuildHouse.Cost.NPC",
-        1000000ULL);
-
-    _portalCost = sConfigMgr->GetOption<uint64_t>(
-        "GuildHouse.Cost.Portal",
-        10000000ULL);
-
-    _objectCost = sConfigMgr->GetOption<uint64_t>(
-        "GuildHouse.Cost.Object",
-        500000ULL);
+    _enabled = sConfigMgr->GetOption<bool>("GuildHouse.Enable", false);
+    _free = sConfigMgr->GetOption<bool>("GuildHouse.Free", false);
+    _loadAll = sConfigMgr->GetOption<bool>("GuildHouse.LoadAll", false);
+    _houseCost = sConfigMgr->GetOption<uint64_t>("GuildHouse.Cost.House", 1000000ULL);
+    _npcCost = sConfigMgr->GetOption<uint64_t>("GuildHouse.Cost.NPC", 1000000ULL);
+    _portalCost = sConfigMgr->GetOption<uint64_t>("GuildHouse.Cost.Portal", 10000000ULL);
+    _objectCost = sConfigMgr->GetOption<uint64_t>("GuildHouse.Cost.Object", 500000ULL);
 }
 
 bool GuildHouseConfig::Enabled() const
