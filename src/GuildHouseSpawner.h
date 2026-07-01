c3a026c9c23bc254d645af361e676a2f610b586e
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-06-30 22:08:42 -0400

    Add GuildHouseSpawner class for guild management

---
+++ src/GuildHouseSpawner.h
@@ -0,0 +1,22 @@
+#ifndef MOD_GUILDHOUSE_SPAWNER_H
+#define MOD_GUILDHOUSE_SPAWNER_H
+
+#include <cstdint>
+
+class GuildHouseSpawner
+{
+public:
+    static GuildHouseSpawner& Instance();
+
+    void SpawnGuild(uint32_t guildId);
+    void DespawnGuild(uint32_t guildId);
+
+private:
+    GuildHouseSpawner() = default;
+
+    void SpawnAsset(uint32_t guildId, uint32_t assetId);
+};
+
+#define sGuildHouseSpawner GuildHouseSpawner::Instance()
+
+#endif
