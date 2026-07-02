09bddf54efea31ab247a1209ddacacf957d9ae96
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-02 05:54:17 -0400

    Add GuildHouseConfig class with configuration methods

---
+++ src/GuildHouseConfig.h
@@ -0,0 +1,37 @@
+#ifndef MOD_GUILDHOUSE_CONFIG_H
+#define MOD_GUILDHOUSE_CONFIG_H
+
+#include <cstdint>
+
+class GuildHouseConfig
+{
+public:
+    static GuildHouseConfig& Instance();
+
+    void Load();
+
+    bool IsEnabled() const;
+    bool IsFree() const;
+    bool LoadAll() const;
+
+    uint64_t GetHouseCost() const;
+    uint64_t GetNpcCost() const;
+    uint64_t GetPortalCost() const;
+    uint64_t GetObjectCost() const;
+
+private:
+    GuildHouseConfig() = default;
+
+    bool _enabled = false;
+    bool _free = false;
+    bool _loadAll = false;
+
+    uint64_t _houseCost = 1000000;
+    uint64_t _npcCost = 1000000;
+    uint64_t _portalCost = 10000000;
+    uint64_t _objectCost = 500000;
+};
+
+#define sGuildHouseConfig GuildHouseConfig::Instance()
+
+#endif
