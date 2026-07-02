8b9a0225c3d4d118fca6217177e1e3fab78c4edf
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-02 06:04:59 -0400

    Add GuildHouseMgr class for managing guild houses

---
+++ src/GuildHouseMgr.h
@@ -0,0 +1,31 @@
+#ifndef MOD_GUILDHOUSE_MGR_H
+#define MOD_GUILDHOUSE_MGR_H
+
+#include <unordered_map>
+
+#include "GuildHouseTypes.h"
+
+class GuildHouseMgr
+{
+public:
+    static GuildHouseMgr& Instance();
+
+    void Load();
+
+    bool HasGuildHouse(uint32_t guildId) const;
+
+    uint32_t GetPhase(uint32_t guildId) const;
+
+    bool CreateGuildHouse(uint32_t guildId, uint32_t ownerGuid);
+
+    const GHGuildHouse* GetGuildHouse(uint32_t guildId) const;
+
+private:
+    GuildHouseMgr() = default;
+
+    std::unordered_map<uint32_t, GHGuildHouse> _houses;
+};
+
+#define sGuildHouseMgr GuildHouseMgr::Instance()
+
+#endif
