133ed46aa51cdff85c371a402c499746a2bc749a
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-02 21:58:12 -0400

    Fix DELETE statement for creature_template_model

--- data/sql/db-world/0002_guildhouse_npc.sql
+++ data/sql/db-world/0002_guildhouse_npc.sql
@@ -3,7 +3,7 @@ DELETE FROM creature_template WHERE entry=900000;
 INSERT INTO creature_template (entry, name, subname, minlevel, maxlevel, faction, npcflag, unit_class, type, speed_walk, speed_run, AIName, ScriptName)
 VALUES (900000, 'Guild House Broker', 'Guild Services', 80, 80, 35, 1, 1, 7, 1, 1.14286, '', 'GuildHouseNpc');
 
-DELETE FROM creature_template_model WHERE entry=900000;
+DELETE FROM creature_template_model WHERE CreatureID=900000;
 
 INSERT INTO creature_template_model (CreatureID, Idx, CreatureDisplayID, DisplayScale, Probability)
 VALUES (900000, 0, 25901, 1.0, 1.0);
