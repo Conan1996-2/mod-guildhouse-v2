baaafd7270a0791d1de58a6c45ffd984ca205e12
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-03 15:55:36 -0400

    Add GuildHouseCommandScript for guild house commands

---
+++ src/GuildHouseCommands.cpp
@@ -0,0 +1,55 @@
+#include "ScriptMgr.h"
+#include "Chat.h"
+#include "ChatCommand.h"
+#include "Player.h"
+
+using namespace Acore::ChatCommands;
+
+class GuildHouseCommandScript : public CommandScript
+{
+public:
+    GuildHouseCommandScript() : CommandScript("GuildHouseCommandScript") { }
+
+    ChatCommandTable GetCommands() const override
+    {
+        static ChatCommandTable addNpcTable =
+        {
+            { "broker", HandleAddBroker, SEC_GAMEMASTER, Console::No },
+            // salesman comes next
+        };
+
+        static ChatCommandTable npcTable =
+        {
+            { "npc", addNpcTable }
+        };
+
+        static ChatCommandTable addTable =
+        {
+            { "add", npcTable }
+        };
+
+        static ChatCommandTable ghTable =
+        {
+            { "gh", addTable },
+            { "guildhouse", addTable }
+        };
+
+        return ghTable;
+    }
+
+    static bool HandleAddBroker(ChatHandler* handler)
+    {
+        Player* player = handler->GetSession()->GetPlayer();
+
+        ChatHandler(player->GetSession()).PSendSysMessage(
+            "GuildHouse: Broker command received.");
+
+        return true;
+    }
+};
+
+void AddGuildHouseScripts()
+{
+    new GuildHouseNpc();
+    new GuildHouseCommandScript();
+}
