5c7dc97320b58c06124b3581bd72d49b6f21fb90
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-06 23:04:59 -0400

    Refactor GuildHouseMgr method signatures for clarity

--- src/GuildHouseMgr.h
+++ src/GuildHouseMgr.h
@@ -3,32 +3,58 @@
 
 #include <unordered_map>
 
-#include "Player.h"
 #include "GuildHouseTypes.h"
+
+
+class Player;
+
 
 class GuildHouseMgr
 {
 public:
+
     static GuildHouseMgr& Instance();
+
 
     void Load();
 
-    bool HasGuildHouse(uint32_t guildId) const;
 
-    uint32_t GetPhase(uint32_t guildId) const;
+    bool HasGuildHouse(
+        uint32_t guildId) const;
 
-    bool CreateGuildHouse(uint32_t guildId, uint32_t ownerGuid);
 
-    const GHGuildHouse* GetGuildHouse(uint32_t guildId) const;
+    const GHGuildHouse* GetGuildHouse(
+        uint32_t guildId) const;
 
-    bool PurchaseCatalogItem(Player* player, uint32_t catalogId);
+
+    uint32_t GetPhase(
+        uint32_t guildId) const;
+
+
+
+    bool CreateGuildHouse(
+        uint32_t guildId,
+        uint32_t ownerGuid);
+
+
+
+    bool PurchaseCatalogItem(
+        Player* player,
+        uint32_t catalogId);
+
+
 
 private:
+
     GuildHouseMgr() = default;
 
+
     std::unordered_map<uint32_t, GHGuildHouse> _houses;
+
 };
 
+
 #define sGuildHouseMgr GuildHouseMgr::Instance()
+
 
 #endif
