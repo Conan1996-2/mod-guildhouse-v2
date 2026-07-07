06968d22d2fcd9fc9781b50d218d2aa881340111
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-06 20:20:14 -0400

    Refactor GHInstanceRecord and GuildHouseInstanceMgr

--- src/GuildHouseInstanceMgr.h
+++ src/GuildHouseInstanceMgr.h
@@ -3,41 +3,54 @@
 
 #include <unordered_map>
 #include <vector>
-#include "GuildHouseDefines.h"
 
 struct GHInstanceRecord
 {
     uint32_t guildId;
+
     uint32_t assetId;
     uint32_t catalogId;
 
     uint32_t guid;
-    uint8_t  type; // 0 creature, 1 gameobject
+
+    uint8_t type;          // creature/gameobject/salesman
 
     uint32_t mapId;
     uint32_t phase;
 
-    float x, y, z, o;
+    float x;
+    float y;
+    float z;
+    float o;
 };
 
 class GuildHouseInstanceMgr
 {
 public:
+
     static GuildHouseInstanceMgr& Instance();
 
     void Load();
+
     void Save(const GHInstanceRecord& record);
 
     void AddInstance(const GHInstanceRecord& record);
-    void RemoveGuild(uint32_t guildId);
-    void LoadInstances();
 
-    const std::vector<GHInstanceRecord>& GetGuildInstances(uint32_t guildId) const;
+    void RemoveGuild(uint32_t guildId);
+
+    void RemoveGuid(uint32_t guid);
+
+    const std::vector<GHInstanceRecord>&
+    GetGuildInstances(uint32_t guildId) const;
+
+    GHInstanceRecord* GetByGuid(uint32_t guid);
 
 private:
+
     GuildHouseInstanceMgr() = default;
 
-    std::unordered_map<uint32_t, std::vector<GHInstanceRecord>> _instances;
+    std::unordered_map<uint32_t,
+        std::vector<GHInstanceRecord>> _instances;
 };
 
 #define sGuildHouseInstanceMgr GuildHouseInstanceMgr::Instance()
