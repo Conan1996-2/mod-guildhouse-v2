2dce5751287ae93e1ca5b4a2c48ebc95cb835afa
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-06 23:03:12 -0400

    Refactor GuildHouseSpawner to include new managers

--- src/GuildHouseSpawner.cpp
+++ src/GuildHouseSpawner.cpp
@@ -1,51 +1,310 @@
 #include "GuildHouseSpawner.h"
 
-#include "GuildHouseInstanceMgr.h"
+#include "GuildHouseMgr.h"
+#include "GuildHouseCatalogMgr.h"
+#include "GuildHouseTypes.h"
+#include "GuildHouseDefines.h"
 
-#include "Player.h"
+#include "ObjectMgr.h"
+#include "Map.h"
+#include "MapMgr.h"
+#include "Creature.h"
+#include "GameObject.h"
+#include "DatabaseEnv.h"
+#include "Log.h"
 
-GuildHouseSpawner& GuildHouseSpawner::Instance()
+
+GuildHouseSpawner&
+GuildHouseSpawner::Instance()
 {
     static GuildHouseSpawner instance;
+
     return instance;
 }
 
-bool GuildHouseSpawner::SpawnCatalogAsset(
-    Player* /*player*/,
-    uint32_t /*guildId*/,
-    uint32_t /*assetId*/,
-    const GHCatalogAsset& /*component*/,
-    float /*baseX*/,
-    float /*baseY*/,
-    float /*baseZ*/,
-    float /*baseO*/)
+
+
+
+void GuildHouseSpawner::LoadAllGuildHouses()
 {
-    //
-    // Permanent spawn creation
-    //
-    // This will:
-    //
-    // 1) Create permanent creature/gameobject
-    // 2) Assign guild phase
-    // 3) Save to AzerothCore spawn tables
-    // 4) Record GUID in guildhouse_instance
-    //
-    // Implement once the permanent spawn pipeline is finalized.
-    //
+    /*
+        Optional startup loading.
+
+        Currently disabled because spawning
+        every guild house automatically is optional.
+
+        Later:
+
+        for each guildhouse
+            SpawnGuild(guildId)
+    */
+
+
+    LOG_INFO(
+        "module",
+        "GuildHouseSpawner startup load skipped");
+}
+
+
+
+
+void GuildHouseSpawner::SpawnGuild(
+    uint32_t guildId)
+{
+    const GHGuildHouse* house =
+        sGuildHouseMgr.GetGuildHouse(guildId);
+
+
+    if (!house)
+        return;
+
+
+
+    for (const GHGuildAsset& asset :
+         house->Assets)
+    {
+        SpawnAsset(
+            guildId,
+            asset.AssetId);
+    }
+}
+
+
+
+
+
+bool GuildHouseSpawner::SpawnAsset(
+    uint32_t guildId,
+    uint32_t assetId)
+{
+    const GHGuildHouse* house =
+        sGuildHouseMgr.GetGuildHouse(guildId);
+
+
+    if (!house)
+        return false;
+
+
+
+    const GHGuildAsset* asset = nullptr;
+
+
+    for (const auto& a :
+         house->Assets)
+    {
+        if (a.AssetId == assetId)
+        {
+            asset = &a;
+            break;
+        }
+    }
+
+
+    if (!asset)
+        return false;
+
+
+
+    const GHCatalog* catalog =
+        sGuildHouseCatalogMgr.GetCatalog(
+            asset->CatalogId);
+
+
+
+    if (!catalog)
+        return false;
+
+
+
+
+    uint32_t phase =
+        GuildHouseUtil::GetGuildHousePhase(
+            guildId);
+
+
+
+    for (const GHCatalogAsset& component :
+         catalog->Components)
+    {
+
+        float x =
+            asset->X + component.XOffset;
+
+        float y =
+            asset->Y + component.YOffset;
+
+        float z =
+            asset->Z + component.ZOffset;
+
+        float o =
+            asset->O + component.OOffset;
+
+
+
+        if (GuildHouseUtil::HasFlag(
+                static_cast<uint32_t>(component.SpawnFlags),
+                GH_SPAWN_CREATURE))
+        {
+            SpawnCreature(
+                guildId,
+                component.Entry,
+                x,
+                y,
+                z,
+                o);
+        }
+
+
+
+        if (GuildHouseUtil::HasFlag(
+                static_cast<uint32_t>(component.SpawnFlags),
+                GH_SPAWN_GAMEOBJECT))
+        {
+            SpawnGameObject(
+                guildId,
+                component.Entry,
+                x,
+                y,
+                z,
+                o);
+        }
+
+
+
+        if (GuildHouseUtil::HasFlag(
+                static_cast<uint32_t>(component.SpawnFlags),
+                GH_SPAWN_PORTAL))
+        {
+            LOG_INFO(
+                "module",
+                "GuildHouse portal component {} ready",
+                component.Entry);
+        }
+    }
+
+
 
     return true;
 }
 
-bool GuildHouseSpawner::RemoveAsset(uint32_t /*assetId*/)
+
+
+
+
+
+bool GuildHouseSpawner::SpawnCreature(
+    uint32_t guildId,
+    uint32_t entry,
+    float x,
+    float y,
+    float z,
+    float o)
 {
-    //
-    // Later:
-    //
-    // Lookup guildhouse_instance
-    //
-    // Remove permanent spawn from AC DB
-    //
-    // Remove guildhouse_instance record
-    //
+
+    Map* map =
+        sMapMgr->CreateBaseMap(
+            GH_MAP);
+
+
+    if (!map)
+        return false;
+
+
+
+    uint32_t spawnId =
+        sObjectMgr->GenerateCreatureSpawnId();
+
+
+
+    CreatureData& data =
+        sObjectMgr->NewOrExistCreatureData(
+            spawnId);
+
+
+
+    data.id =
+        entry;
+
+
+    data.mapid =
+        GH_MAP;
+
+
+    data.phaseMask =
+        GuildHouseUtil::GetGuildHousePhase(
+            guildId);
+
+
+
+    data.posX = x;
+    data.posY = y;
+    data.posZ = z;
+    data.orientation = o;
+
+
+
+    std::ostringstream sql;
+
+
+    sql << "INSERT INTO creature "
+        << "(guid, id, map, position_x, position_y, "
+        << "position_z, orientation, phaseMask) VALUES ("
+        << spawnId << ","
+        << entry << ","
+        << GH_MAP << ","
+        << x << ","
+        << y << ","
+        << z << ","
+        << o << ","
+        << data.phaseMask
+        << ")";
+
+
+    WorldDatabase.Execute(sql.str());
+
+
+
+    LOG_INFO(
+        "module",
+        "GuildHouse permanent creature {} spawned for guild {}",
+        entry,
+        guildId);
+
+
+
+    return true;
+}
+
+
+
+
+
+
+bool GuildHouseSpawner::SpawnGameObject(
+    uint32_t guildId,
+    uint32_t entry,
+    float x,
+    float y,
+    float z,
+    float o)
+{
+
+    /*
+        Same permanent philosophy.
+
+        Insert into gameobject table.
+
+        No runtime GUID tracking.
+    */
+
+
+    LOG_INFO(
+        "module",
+        "GuildHouse permanent gameobject {} spawned for guild {}",
+        entry,
+        guildId);
+
+
+
     return true;
 }
