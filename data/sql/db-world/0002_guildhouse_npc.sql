74db331242afa4dcae7d1fdeda9a3503617d8553
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-07 21:31:27 -0400

    Update NPC script name for Guild House Broker

--- data/sql/db-world/0002_guildhouse_npc.sql
+++ data/sql/db-world/0002_guildhouse_npc.sql
@@ -1,7 +1,7 @@
 DELETE FROM creature_template WHERE entry=900000;
 
 INSERT INTO creature_template (entry, name, subname, minlevel, maxlevel, faction, npcflag, unit_class, type, speed_walk, speed_run, AIName, ScriptName)
-VALUES (900000, 'Guild House Broker', 'Guild Services', 80, 80, 35, 1, 1, 7, 1, 1.14286, '', 'GuildHouseNpc');
+VALUES (900000, 'Guild House Broker', 'Guild Services', 80, 80, 35, 1, 1, 7, 1, 1.14286, '', 'GuildHouseBroker');
 
 DELETE FROM creature_template_model WHERE CreatureID=900000;
 
