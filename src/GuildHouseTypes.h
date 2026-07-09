c129228c98049983146509326139974ad1c1b1dd
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-09 14:58:15 -0400

    Remove unused fields and clean up struct definitions

--- src/GuildHouseTypes.h
+++ src/GuildHouseTypes.h
@@ -7,7 +7,6 @@
 
 #include "GuildHouseDefines.h"
 
-
 // =====================================================
 // Catalog Category
 // =====================================================
@@ -16,14 +15,10 @@ struct GHCategory
 {
     uint32_t Id;
     uint32_t ParentId;
-
     std::string Name;
-
     uint16_t SortOrder;
-
     bool Enabled;
 };
-
 
 // =====================================================
 // Catalog Component
@@ -34,57 +29,27 @@ struct GHCategory
 struct GHCatalogAsset
 {
     uint32_t ComponentId;
-
     uint32_t CatalogId;
 
-
-    // Spawn information
-
     uint32_t Entry;
-
     uint32_t DisplayId;
-
     float Scale;
-
-
     GHSpawnFlags SpawnFlags;
-
     GHBehaviorFlags BehaviorFlags;
-
-
-    // Runtime script
-
     GHScriptType ScriptType;
-
     std::string ScriptData;
-
-
-    // Placement offset from purchased asset
-
     float XOffset;
     float YOffset;
     float ZOffset;
     float OOffset;
-
-
-    // Portal / destination data
-
     uint32_t TargetMap;
-
     float TargetX;
     float TargetY;
     float TargetZ;
     float TargetO;
-
-
-    // Nested catalog item
-
     uint32_t ChildCatalogId;
-
-
     uint16_t SortOrder;
 };
-
 
 // =====================================================
 // Catalog Item
@@ -95,24 +60,13 @@ struct GHCatalogAsset
 struct GHCatalog
 {
     uint32_t CatalogId;
-
     uint32_t CategoryId;
-
-
     std::string Name;
-
-
     GHSpawnFlags SpawnFlags;
-
     GHBehaviorFlags BehaviorFlags;
-
-
     bool Enabled;
-
-
     std::vector<GHCatalogAsset> Components;
 };
-
 
 // =====================================================
 // Purchased Guild Asset
@@ -123,69 +77,40 @@ struct GHCatalog
 struct GHGuildAsset
 {
     uint32_t AssetId;
-
     uint32_t GuildId;
-
     uint32_t CatalogId;
-
-
     uint16_t LayoutId;
-
-
     GHAssetStatus Status;
-
-
-    uint32_t Phase;
-
-
+    uint32_t InstanceId;
     float X;
     float Y;
     float Z;
     float O;
 };
-
 
 // =====================================================
 // Permanent Spawn Registry
 //
 // Stored in guildhouse_instance
-//
-// This is NOT a temporary runtime instance.
-// It tracks permanent world objects.
 // =====================================================
 
 struct GHInstance
 {
     uint32_t InstanceId;
-
-
     uint32_t GuildId;
-
-
     uint32_t AssetId;
-
     uint32_t CatalogId;
-
-
     uint32_t Guid;
-
-
     uint8_t Type;
     // 0 = creature
     // 1 = gameobject
-
-
     uint32_t MapId;
-
-    uint32_t Phase;
-
-
+    uint32_t InstanceId;
     float X;
     float Y;
     float Z;
     float O;
 };
-
 
 // =====================================================
 // Guild House
@@ -196,15 +121,9 @@ struct GHInstance
 struct GHGuildHouse
 {
     uint32_t GuildId;
-
     uint32_t OwnerGuid;
-
-
-    uint32_t Phase;
-
-
+    uint32_t InstanceId;
     std::vector<GHGuildAsset> Assets;
 };
-
 
 #endif
