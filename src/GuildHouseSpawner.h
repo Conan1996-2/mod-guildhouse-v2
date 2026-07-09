0248551bd68ddd21aae0680a7117a8d1da35cfd3
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-09 00:18:29 -0400

    Refactor SpawnAsset and related methods for clarity

--- src/GuildHouseSpawner.h
+++ src/GuildHouseSpawner.h
@@ -8,43 +8,26 @@ class GuildHouseSpawner
 public:
 
     static GuildHouseSpawner& Instance();
+
     void LoadPlacedAssets();
 
+    bool HasExistingSpawn(uint32 assetId);
 
     // Spawn one purchased catalog item
-    bool SpawnAsset(
-        uint32_t guildId,
-        uint32_t assetId);
+    bool SpawnAsset(uint32_t guildId, uint32_t assetId);
 
-    bool RemoveAsset(
-        uint32_t guildId,
-        uint32_t assetId);
+    bool RemoveAsset(uint32_t guildId, uint32_t assetId);
 
 private:
 
     GuildHouseSpawner() = default;
 
-    bool SpawnCreature(
-        uint32_t guildId,
-        uint32_t assetId,
-        uint32_t entry,
-        float x,
-        float y,
-        float z,
-        float o);
+    bool SpawnCreature(uint32_t guildId, uint32_t assetId, uint32_t entry, float x, float y, float z, float o);
 
-    bool SpawnGameObject(
-        uint32_t guildId,
-        uint32_t assetId,
-        uint32_t entry,
-        float x,
-        float y,
-        float z,
-        float o);
+    bool SpawnGameObject( uint32_t guildId, uint32_t assetId, uint32_t entry, float x, float y, float z, float o);
 
 };
 
 #define sGuildHouseSpawner GuildHouseSpawner::Instance()
-
 
 #endif
