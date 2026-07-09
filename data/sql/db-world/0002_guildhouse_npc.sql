2d826abe55dc3f2a32ba34337bf15b05afdbc3c0
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-09 11:57:29 -0400

    Insert Guild House Broker NPCs into database
    Added entries for Guild House Broker NPCs with IDs 900001, 900002, and 900003.

--- data/sql/db-world/0002_guildhouse_npc.sql
+++ data/sql/db-world/0002_guildhouse_npc.sql
@@ -7,3 +7,33 @@ DELETE FROM creature_template_model WHERE CreatureID=900000;
 
 INSERT INTO creature_template_model (CreatureID, Idx, CreatureDisplayID, DisplayScale, Probability)
 VALUES (900000, 0, 25901, 1.0, 1.0);
+
+DELETE FROM creature_template WHERE entry=900001;
+
+INSERT INTO creature_template (entry, name, subname, minlevel, maxlevel, faction, npcflag, unit_class, type, speed_walk, speed_run, AIName, ScriptName)
+VALUES (900001, 'Guild House Broker', 'Guild Services', 80, 80, 35, 1, 1, 7, 1, 1.14286, '', 'GuildHouseBroker');
+
+DELETE FROM creature_template_model WHERE CreatureID=900001;
+
+INSERT INTO creature_template_model (CreatureID, Idx, CreatureDisplayID, DisplayScale, Probability)
+VALUES (900001, 0, 25901, 1.0, 1.0);
+
+DELETE FROM creature_template WHERE entry=900002;
+
+INSERT INTO creature_template (entry, name, subname, minlevel, maxlevel, faction, npcflag, unit_class, type, speed_walk, speed_run, AIName, ScriptName)
+VALUES (900002, 'Guild House Broker', 'Guild Services', 80, 80, 35, 1, 1, 7, 1, 1.14286, '', 'GuildHouseBroker');
+
+DELETE FROM creature_template_model WHERE CreatureID=900002;
+
+INSERT INTO creature_template_model (CreatureID, Idx, CreatureDisplayID, DisplayScale, Probability)
+VALUES (900002, 0, 25901, 1.0, 1.0);
+
+DELETE FROM creature_template WHERE entry=900003;
+
+INSERT INTO creature_template (entry, name, subname, minlevel, maxlevel, faction, npcflag, unit_class, type, speed_walk, speed_run, AIName, ScriptName)
+VALUES (900003, 'Guild House Broker', 'Guild Services', 80, 80, 35, 1, 1, 7, 1, 1.14286, '', 'GuildHouseBroker');
+
+DELETE FROM creature_template_model WHERE CreatureID=900003;
+
+INSERT INTO creature_template_model (CreatureID, Idx, CreatureDisplayID, DisplayScale, Probability)
+VALUES (900003, 0, 25901, 1.0, 1.0);
