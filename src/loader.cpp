1358b4558e801eaa5e2f66a4183c06cb2a7a5fd1
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-10 22:03:36 -0400

    Add GuildHouseMgr script to GuildHouseScripts

--- src/loader.cpp
+++ src/loader.cpp
@@ -3,10 +3,12 @@
 void AddSC_GuildHouseBroker();
 void AddSC_GuildHouseSalesman();
 void AddSC_GuildHouseCommands();
+void AddSC_GuildHouseMgr();
 
 void AddGuildHouseScripts()
 {
     AddSC_GuildHouseBroker();
     AddSC_GuildHouseSalesman();
     AddSC_GuildHouseCommands();
+    AddSC_GuildHouseMgr();
 }
