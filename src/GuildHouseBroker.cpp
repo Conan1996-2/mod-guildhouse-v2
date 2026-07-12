72568d95df5d2a1c2326350b320af393542f0dd9
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-11 20:13:41 -0400

    Fix syntax for getting guild house and location

--- src/GuildHouseBroker.cpp
+++ src/GuildHouseBroker.cpp
@@ -182,15 +182,14 @@ bool GuildHouseBroker::OnGossipSelect(Player* player, Creature* creature, uint32
                 break;
             }
 
-            GHGuildHouse* guild GetGuildHouse (guildId);
-            const GHLocation* location = sGuildHouseMgr.GetLocation(guild.InstanceId);
-            
             if (!sGuildHouseMgr.SellGuildHouse(guildId))
             {
                 ChatHandler(player->GetSession()).PSendSysMessage("Failed to sell Guild House.");
                 break;
             }
 
+            const GHGuildHouse* guild GetGuildHouse (guildId);
+            const GHLocation* location = sGuildHouseMgr.GetLocation(guild.InstanceId);
             uint64 refund = location->Price * sGuildHouseConfig.GetRefundPercent() / 100;
             player->ModifyMoney(refund);
             
