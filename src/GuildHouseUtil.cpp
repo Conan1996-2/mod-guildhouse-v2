749b37226161d9f23c6f05cd8e460679e93413fb
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-15 22:56:42 -0400

    Add IsInGuildHouse function to check player location

---
+++ src/GuildHouseUtil.cpp
@@ -0,0 +1,36 @@
+    inline bool IsInGuildHouse(Player* player)
+    {
+        if (!player)
+            return false;
+
+        Guild* guild = player->GetGuild();
+        if (!guild)
+            return false;
+    
+        const GHGuildHouse* house = sGuildHouseMgr.GetGuildHouse(guild->GetId());
+        if (!house)
+            return false;
+    
+        const GHLocation* location = sGuildHouseMgr.GetLocation(house->LocationId);
+        if (!location)
+            return false;
+        
+        if (player->GetMapId() != location->MapId)
+            return false;
+
+        float x = player->GetPositionX();
+        float y = player->GetPositionY();
+        if (x < location->MinX || x > location->MaxX || y < location->MinY || y > location->MaxY)
+            return false;
+
+        return sGuildHouseMgr.IsGuildInstance(guild->GetId(), player->GetInstanceId());
+        
+        //uint32 instanceId = player->GetInstanceId();
+        //if (!instanceId)
+        //    return false;
+
+        //return player->GetInstanceId() == sGuildHouseMgr.GetGuildInstance(guild->GetId());
+
+       // return IsGuildHouseInstance(guild->GetId(), instanceId);
+    }
+
