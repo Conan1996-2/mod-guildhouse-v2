5ec98b3148e0fd8d00974476a270fc8ce91d3542
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-16 13:34:20 -0400

    Refactor GuildHouseConfig by removing house cost
    Removed house cost member variable and initialized other cost variables.

--- src/GuildHouseConfig.h
+++ src/GuildHouseConfig.h
@@ -6,6 +6,7 @@
 class GuildHouseConfig
 {
 public:
+
     static GuildHouseConfig& Instance();
 
     void Load();
@@ -14,26 +15,30 @@ public:
     bool IsFree() const;
     bool LoadAll() const;
 
-    uint64_t GetHouseCost() const;
     uint64_t GetNpcCost() const;
     uint64_t GetPortalCost() const;
     uint64_t GetObjectCost() const;
     uint64_t GetRefundPercent() const;
 
+
 private:
+
     GuildHouseConfig() = default;
+
 
     bool _enabled = false;
     bool _free = false;
     bool _loadAll = false;
 
-    uint64_t _houseCost;
-    uint64_t _npcCost;
-    uint64_t _portalCost;
-    uint64_t _objectCost;
-    uint64_t _refundPercent;
+
+    uint64_t _npcCost = 0;
+    uint64_t _portalCost = 0;
+    uint64_t _objectCost = 0;
+    uint64_t _refundPercent = 80;
 };
 
+
 #define sGuildHouseConfig GuildHouseConfig::Instance()
+
 
 #endif
