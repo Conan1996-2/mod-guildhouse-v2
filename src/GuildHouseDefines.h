3ca7d09d90ed38727f1c0def167ee5fad2026eb6
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-15 22:54:54 -0400

    Refactor IsInGuildHouse to a function declaration
    Replaced the implementation of IsInGuildHouse with a declaration.

--- src/GuildHouseDefines.h
+++ src/GuildHouseDefines.h
@@ -153,41 +153,7 @@ namespace GuildHouseUtil
         return HasFlag(flags, GH_FACTION_NEUTRAL);
     }
 
-    inline bool IsInGuildHouse(Player* player)
-    {
-        if (!player)
-            return false;
-
-        Guild* guild = player->GetGuild();
-        if (!guild)
-            return false;
-    
-        const GHGuildHouse* house = sGuildHouseMgr.GetGuildHouse(guild->GetId());
-        if (!house)
-            return false;
-    
-        const GHLocation* location = sGuildHouseMgr.GetLocation(house->LocationId);
-        if (!location)
-            return false;
-        
-        if (player->GetMapId() != location->MapId)
-            return false;
-
-        float x = player->GetPositionX();
-        float y = player->GetPositionY();
-        if (x < location->MinX || x > location->MaxX || y < location->MinY || y > location->MaxY)
-            return false;
-
-        return sGuildHouseMgr.IsGuildInstance(guild->GetId(), player->GetInstanceId());
-        
-        //uint32 instanceId = player->GetInstanceId();
-        //if (!instanceId)
-        //    return false;
-
-        //return player->GetInstanceId() == sGuildHouseMgr.GetGuildInstance(guild->GetId());
-
-       // return IsGuildHouseInstance(guild->GetId(), instanceId);
-    }
+bool IsInGuildHouse(Player* player);
 
     inline bool IsGuildMaster(Player* player)
     {
