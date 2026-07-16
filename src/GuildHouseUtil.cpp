06a36d0b7c7493d6a32307c00dd8d88d30cc3e68
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-15 23:41:51 -0400

    Enhance guild house validation messages
    Added error messages for various guild house checks.

--- src/GuildHouseUtil.cpp
+++ src/GuildHouseUtil.cpp
@@ -12,27 +12,45 @@ namespace GuildHouseUtil
     bool IsInGuildHouse(Player* player)
     {
         if (!player)
+        {
+            ChatHandler(player->GetSession()).PSendSysMessage("Yournot a player.");
             return false;
+        }
 
         Guild* guild = player->GetGuild();
         if (!guild)
+        {
+            ChatHandler(player->GetSession()).PSendSysMessage("Yournot in a guild.");
             return false;
+        }
     
         const GHGuildHouse* house = sGuildHouseMgr.GetGuildHouse(guild->GetId());
         if (!house)
+        {
+            ChatHandler(player->GetSession()).PSendSysMessage("Your guild does not own a Guild House.");
             return false;
+        }
     
         const GHLocation* location = sGuildHouseMgr.GetLocation(house->LocationId);
         if (!location)
+        {
+            ChatHandler(player->GetSession()).PSendSysMessage("Your guild house is not a valid id.");
             return false;
+        }
         
         if (player->GetMapId() != location->MapId)
+        {
+            ChatHandler(player->GetSession()).PSendSysMessage("Your not in your guild house.");
             return false;
+        }
 
         float x = player->GetPositionX();
         float y = player->GetPositionY();
         if (x < location->MinX || x > location->MaxX || y < location->MinY || y > location->MaxY)
+        {
+            ChatHandler(player->GetSession()).PSendSysMessage("Your not standing in the guild house boundries.");
             return false;
+        }
 
         return sGuildHouseMgr.IsGuildInstance(guild->GetId(), player->GetInstanceId());
         
