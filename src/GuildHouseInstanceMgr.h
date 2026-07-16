8b54c23ef3a1a43674dd9be6f6773a66a3649569
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-16 13:56:23 -0400

    Refactor GuildHouseInstanceMgr methods and members

--- src/GuildHouseInstanceMgr.h
+++ src/GuildHouseInstanceMgr.h
@@ -10,11 +10,6 @@ struct GHInstanceRecord
     uint32_t GuildId = 0;
 
     uint32_t MapId = 0;
-
-    float X = 0;
-    float Y = 0;
-    float Z = 0;
-    float O = 0;
 };
 
 class GuildHouseInstanceMgr
@@ -26,31 +21,42 @@ public:
     /*
         Instance lifecycle
     */
-
     void Load();
-    uint32_t CreateInstance(uint32_t guildId);
+
+    uint32_t CreateInstance(uint32_t guildId, uint32_t mapId);
+
     bool RemoveInstance(uint32_t guildId);
+
+    bool RemoveInstanceById(uint32_t instanceId);
 
     /*
         Lookup
     */
 
     uint32_t GetInstanceId(uint32_t guildId) const;
+
+    uint32_t GetGuildId(uint32_t instanceId) const;
+
     bool HasInstance(uint32_t guildId) const;
+
     bool IsGuildInstance(uint32_t guildId, uint32_t instanceId) const;
 
     /*
         Runtime tracking
     */
-
     const GHInstanceRecord* GetInstance(uint32_t instanceId) const;
 
 private:
 
     GuildHouseInstanceMgr() = default;
+
     uint32_t GenerateInstanceId();
+
     std::unordered_map<uint32_t, GHInstanceRecord> _instances;
+
     std::unordered_map<uint32_t, uint32_t> _guildInstances;
+
+    std::unordered_map<uint32_t, uint32_t> _instanceGuilds;
 };
 
 #define sGuildHouseInstanceMgr GuildHouseInstanceMgr::Instance()
