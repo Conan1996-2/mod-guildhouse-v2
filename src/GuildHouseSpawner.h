100438abd51859479081f012089ef0566aa063a6
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-16 14:15:35 -0400

    Refactor Spawn methods to add mapId parameter
    Updated SpawnCreature and SpawnGameObject methods to include mapId parameter.

--- src/GuildHouseSpawner.h
+++ src/GuildHouseSpawner.h
@@ -3,30 +3,75 @@
 
 #include <cstdint>
 
+
 class GuildHouseSpawner
 {
 public:
 
     static GuildHouseSpawner& Instance();
 
+
+    /*
+        Startup loading
+    */
+
     void LoadPlacedAssets();
+
+
+    /*
+        Spawn tracking
+    */
 
     bool HasExistingSpawn(uint32_t instanceId, uint32_t assetId);
 
+
+    /*
+        Asset spawning
+    */
+
     bool SpawnAsset(uint32_t guildId, uint32_t assetId);
 
+
+    /*
+        Asset removal
+    */
+
     bool RemoveAsset(uint32_t guildId, uint32_t instanceId, uint32_t assetId);
+
     bool RemoveAllAssets(uint32_t guildId, uint32_t instanceId);
+
 
 private:
 
     GuildHouseSpawner() = default;
 
-    bool SpawnCreature(uint32_t guildId, uint32_t instanceId, uint32_t assetId, uint32_t entry, float x, float y, float z, float o);
 
-    bool SpawnGameObject(uint32_t guildId, uint32_t instanceId, uint32_t assetId, uint32_t entry, float x, float y, float z, float o);
+    bool SpawnCreature(
+        uint32_t guildId,
+        uint32_t instanceId,
+        uint32_t assetId,
+        uint32_t mapId,
+        uint32_t entry,
+        float x,
+        float y,
+        float z,
+        float o);
+
+
+    bool SpawnGameObject(
+        uint32_t guildId,
+        uint32_t instanceId,
+        uint32_t assetId,
+        uint32_t mapId,
+        uint32_t entry,
+        float x,
+        float y,
+        float z,
+        float o);
 };
 
+
 #define sGuildHouseSpawner GuildHouseSpawner::Instance()
+
 
 #endif
