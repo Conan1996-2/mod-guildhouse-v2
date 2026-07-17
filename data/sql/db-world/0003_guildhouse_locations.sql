cd16c7f7bb7ebf28875c1af8a2d21e22c8dbbb12
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-17 16:03:29 -0400

    Update 0003_guildhouse_locations.sql

--- data/sql/db-world/0003_guildhouse_locations.sql
+++ data/sql/db-world/0003_guildhouse_locations.sql
@@ -25,7 +25,7 @@ CREATE TABLE `guildhouse_locations`
     KEY `idx_enabled` (`enabled`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO `guildhouse_locations` (`name`,`mapId`,`positionX`,`positionY`,`positionZ`,`orientation`,`minX`,`maxX`,`minY`,`maxY`,`price`,`enabled`) VALUES
+INSERT INTO `guildhouse_locations` (`name`,`mapId`,`zoneId`,`areaId`,`positionX`,`positionY`,`positionZ`,`orientation`,`minX`,`maxX`,`minY`,`maxY`,`price`,`enabled`) VALUES
 ('GM Island', 1, 876, 876, 16222.57, 16265.91, 13.21, 0, 16000, 16500, 16000, 16500, 1000000, 1),
 ('Valormok', 37, 268, 268, 88.38, 915.82, 338.12, 0, -200, 460, 460, 1250, 10000000, 1),
 ('Talrendis Outpost', 37, 268, 268, 1144.43, 240, 353, 0, 16000, 16500, 16000, 16500, 10000000, 1);
