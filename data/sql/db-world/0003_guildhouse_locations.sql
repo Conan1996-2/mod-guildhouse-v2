6f5015c57b4dd3b5b96a876b375b7628d50c0739
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-17 15:57:02 -0400

    Fix syntax error in Valormok SQL insert statement

--- data/sql/db-world/0003_guildhouse_locations.sql
+++ data/sql/db-world/0003_guildhouse_locations.sql
@@ -27,5 +27,5 @@ CREATE TABLE `guildhouse_locations`
 
 INSERT INTO `guildhouse_locations` (`name`,`mapId`,`positionX`,`positionY`,`positionZ`,`orientation`,`minX`,`maxX`,`minY`,`maxY`,`price`,`enabled`) VALUES
 ('GM Island', 1, 876, 876, 16222.57, 16265.91, 13.21, 0, 16000, 16500, 16000, 16500, 1000000, 1),
-('Valormok', 37, 268, 268 88.38, 915.82, 338.12, 0, -200, 460, 460, 1250, 10000000, 1),
+('Valormok', 37, 268, 268, 88.38, 915.82, 338.12, 0, -200, 460, 460, 1250, 10000000, 1),
 ('Talrendis Outpost', 37, 268, 268, 1144.43, 240, 353, 0, 16000, 16500, 16000, 16500, 10000000, 1);
