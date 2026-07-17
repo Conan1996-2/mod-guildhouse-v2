86b7c5adef6a894dc2ba698671500ef2edfe1aae
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-17 01:00:24 -0400

    Refactor GuildHouseSalesman class method signatures

--- src/GuildHouseSalesman.h
+++ src/GuildHouseSalesman.h
@@ -3,27 +3,56 @@
 
 #include "ScriptMgr.h"
 
+
 class Player;
 class Creature;
+
 
 class GuildHouseSalesman : public CreatureScript
 {
 public:
 
-    GuildHouseSalesman() : CreatureScript("GuildHouseSalesman")
+    GuildHouseSalesman()
+        : CreatureScript("GuildHouseSalesman")
     {
     }
 
-    bool OnGossipHello(Player* player, Creature* creature) override;
-    bool OnGossipSelect(Player* player, Creature* creature, uint32 sender, uint32 action) override;
+
+    bool OnGossipHello(
+        Player* player,
+        Creature* creature) override;
+
+
+
+    bool OnGossipSelect(
+        Player* player,
+        Creature* creature,
+        uint32 sender,
+        uint32 action) override;
+
+
 
 private:
 
-    void SendCatalogMenu(Player* player, Creature* creature);
-    void SendCategoryMenu(Player* player, Creature* creature, uint32 categoryId);
 
-    bool ValidateSalesmanAccess(Player* player, Creature* creature);
+    void SendCatalogMenu(
+        Player* player,
+        Creature* creature);
+
+
+
+    void SendCategoryMenu(
+        Player* player,
+        Creature* creature,
+        uint32 categoryId);
+
+
+
+    bool ValidateSalesmanAccess(
+        Player* player,
+        Creature* creature);
 
 };
+
 
 #endif
