2ca24722424cd446b06f2a8e89bc29f98f33eb7b
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-17 11:31:29 -0400

    Clean up GuildHouseCommands.h by removing blank lines
    Removed unnecessary blank lines for cleaner code.

--- src/GuildHouseCommands.h
+++ src/GuildHouseCommands.h
@@ -13,56 +13,38 @@ public:
 
     ChatCommandTable GetCommands() const override;
 
-
     // =====================================================
     // NPC MANAGEMENT
     // =====================================================
-
     static bool HandleAddBroker(ChatHandler* handler);
-
     static bool HandleAddSalesman(ChatHandler* handler);
-
 
     // =====================================================
     // GUILD HOUSE MANAGEMENT
     // =====================================================
-
     static bool HandleSellGuildHouse(ChatHandler* handler, char const* args);
-
     static bool HandleTeleportGuildHouse(ChatHandler* handler, char const* args);
-
 
     // =====================================================
     // ASSET MANAGEMENT
     // =====================================================
-
     static bool HandleListAssets(ChatHandler* handler, char const* args);
-
     static bool HandlePlaceAsset(ChatHandler* handler, char const* args);
-
     static bool HandleMoveAsset(ChatHandler* handler, char const* args);
-
     static bool HandleStoreAsset(ChatHandler* handler, char const* args);
-
     static bool HandleSellAsset(ChatHandler* handler, char const* args);
-
 
     // =====================================================
     // CATALOG / SHOP
     // =====================================================
-
     static bool HandleListCategories(ChatHandler* handler, char const* args);
-
     static bool HandleListCatalog(ChatHandler* handler, char const* args);
-
     static bool HandlePurchaseCatalog(ChatHandler* handler, char const* args);
 };
-
 
 // =====================================================
 // Script registration
 // =====================================================
-
 void AddSC_GuildHouseCommands();
 
 
