bd8d881a225332e705bd1c1d3b72a7e04a4b27a4
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-17 01:09:14 -0400

    Clean up GuildHouse script function calls
    Removed unused function calls related to GuildHouse.

--- src/loader.cpp
+++ src/loader.cpp
@@ -3,16 +3,10 @@
 
 void AddSC_GuildHouseWorld();
 
-void AddSC_GuildHouseMgr();
-void AddSC_GuildHousePhaseMgr();
-void AddSC_GuildHouseInstanceMgr();
-void AddSC_GuildHouseSpawner();
-
 void AddSC_GuildHouseBroker();
 void AddSC_GuildHouseSalesman();
 void AddSC_GuildHouseCommands();
-
-
+void AddSC_GuildHouseMgr();
 
 void AddGuildHouseScripts()
 {
@@ -21,21 +15,15 @@ void AddGuildHouseScripts()
     //
     AddSC_GuildHouseWorld();
 
-
     //
     // Systems
     //
-    AddSC_GuildHousePhaseMgr();
-    AddSC_GuildHouseInstanceMgr();
     AddSC_GuildHouseMgr();
-    AddSC_GuildHouseSpawner();
-
 
     //
     // Gameplay
     //
     AddSC_GuildHouseCommands();
-
 
     //
     // NPCs
