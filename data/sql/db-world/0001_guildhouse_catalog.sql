aac3b96be6966486b09baf82295d4670e37044e5
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-17 20:02:10 -0400

    Add initial data for guildhouse categories and trainers

--- data/sql/db-world/0001_guildhouse_catalog.sql
+++ data/sql/db-world/0001_guildhouse_catalog.sql
@@ -76,3 +76,38 @@ CREATE TABLE `guildhouse_catalog_asset`
 ) ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `guildhouse_category` (`categoryId`, `parentId`, `name`, `sortOrder`, `enabled`) VALUES
+    (100, 0, "NPC", 0, 1),
+    (200, 0, "Portals", 0, 1),
+    (300, 0, "Furnature", 0, 1),
+    (400, 0, "Plants", 0, 1),
+    (101, 100, "Class Trainers", 0, 1),
+    (102, 100, "Profession Trainers", 0, 1),
+    (103, 100, "Other Trainers", 0, 1),
+    (104, 100, "Vendors", 0, 1),
+    (301, 300, "Chairs", 0, 1),
+    (302, 300, "Tables", 0, 1),
+    (303, 300, "Chests", 0, 1;
+    
+INSERT INTO `guildhouse_category` (`catalogId`, `categoryId`, `name`, `price`, `spawnFlags`, `behaviorFlags`, `enabled`) VALUES
+    (1, 101, "Death Knight Trainer", 500000, 1, 4, 1),
+    (2, 101, "Druid Trainer", 500000, 1, 1, 1),
+    (3, 101, "Druid Trainer", 500000, 1, 2, 1),
+    (4, 101, "Hunter Trainer", 500000, 1, 1, 1),
+    (5, 101, "Hunter Trainer", 500000, 1, 2, 1),
+    (6, 101, "Mage Trainer", 500000, 1, 1, 1),
+    (7, 101, "Mage Trainer", 500000, 1, 2, 1),
+    (8, 101, "Paladin Trainer", 500000, 1, 1, 1),
+    (9, 101, "Paladin Trainer", 500000, 1, 2, 1),
+    (10, 101, "Priest Trainer", 500000, 1, 1, 1),
+    (11, 101, "Priest Trainer", 500000, 1, 2, 1),
+    (12, 101, "Rogue Trainer", 500000, 1, 1, 1),
+    (13, 101, "Rogue Trainer", 500000, 1, 2, 1),
+    (14, 101, "Shaman Trainer", 500000, 1, 1, 1),
+    (15, 101, "Shaman Trainer", 500000, 1, 2, 1),
+    (16, 101, "Warrior Trainer", 500000, 1, 1, 1),
+    (17, 101, "Warrior Trainer", 500000, 1, 2, 1),
+    (18, 101, "Warlock Trainer", 500000, 1, 1, 1),
+    (19, 101, "Warlock Trainer", 500000, 1, 2, 1);
+
