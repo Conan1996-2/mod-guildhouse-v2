247df1a16e1f7c2c8a873e9eeee10141d48ebd1b
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-23 00:26:37 -0400

    Add RemoveCreatureSpawn and RemoveGameObjectSpawn methods

--- src/GuildHouseSpawner.h
+++ src/GuildHouseSpawner.h
@@ -36,7 +36,8 @@ private:
     GuildHouseSpawner() = default;
 
     bool SpawnGameObject(uint32_t guildId, uint32_t assetId, uint32_t phaseMask, uint32_t mapId, uint32_t entry, float x, float y, float z, float o);
-
+    bool GuildHouseSpawner::RemoveCreatureSpawn(uint32 guid);
+    bool GuildHouseSpawner::RemoveGameObjectSpawn(uint32 guid);
 };
 
 #define sGuildHouseSpawner GuildHouseSpawner::Instance()
