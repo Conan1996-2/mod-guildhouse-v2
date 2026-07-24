496bb580374512a011b46bfd9d7a0aab8f680ff1
Conan1996-2 <71103778+Conan1996-2@users.noreply.github.com>
2026-07-24 13:11:59 -0400

    Implement GuildHousePlayerScript for player management

---
+++ src/GuildHousePlayerScript.cpp
@@ -0,0 +1,31 @@
+#include "Player.h"
+#include "PlayerScript.h"
+#include "GuildHousePhaseMgr.h"
+
+class GuildHousePlayerScript : public PlayerScript
+{
+public:
+    GuildHousePlayerScript() : PlayerScript("GuildHousePlayerScript") { }
+
+    void OnUpdate(Player* player, uint32 /*diff*/) override
+    {
+        sGuildHousePhaseMgr.CheckBoundary(player);
+    }
+
+    void OnMapChanged(Player* player) override
+    {
+        if (sGuildHousePhaseMgr.IsMember(player))
+            sGuildHousePhaseMgr.LeavePhase(player);
+    }
+
+    void OnLogout(Player* player) override
+    {
+        if (sGuildHousePhaseMgr.IsMember(player))
+            sGuildHousePhaseMgr.LeavePhase(player);
+    }
+};
+
+void AddGuildHouseScripts()
+{
+    new GuildHousePlayerScript();
+}
